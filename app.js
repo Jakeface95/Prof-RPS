5f5da5dae6a117909468dcd7c5cd8f3a9d5d1b40
Jake Williams <jake5195@hotmail.co.uk>
2020-09-30 16:01:50 +0100

    Working loop - full function not complete

--- app.js
+++ app.js
@@ -5,11 +5,33 @@ function playerMoveSelect() {
 function compMoveSelect() {
     let compVals = ["p", "r", "s"];
     let randomVal = Math.floor(Math.random() * compVals.length);
-    let compVal = compVals[randomVal]
-    return compVal
+    let compMove = compVals[randomVal]
+    return compMove
 }
 
+function compareMoves(playerMove, compMove) {
+    if (playerMove === "r" && compMove === "s" || playerMove === "s" && compMove === "p" || playerMove === "p" && compMove === "r") {
+        return playerScore ++
+    } else if (playerMove === "r" && compMove === "p" || playerMove === "s" && compMove === "r" || playerMove === "p" && compMove === "s") {
+        return compScore ++
+    } 
+}
+
+// function rockPaperScissorsGame(playerScore, compScore) {
+    let playerScore = 0;
+    let compScore = 0;
+    while (playerScore < 3 && compScore < 3) {
+        compareMoves(playerMoveSelect(), compMoveSelect())
+        alert(playerScore + " - " + compScore)
+    }
+    if (playerScore === 3) {
+        alert(`You have won, ${playerScore} - ${compScore}`); 
+    } else if (compScore === 3) {
+        alert(`You have lost, ${playerScore} - ${compScore}`);
+    }
+// }
+
+// rockPaperScissorsGame(playerScore, compScore)
 
 
-console.log(playerMoveSelect());
-console.log(compMoveSelect());
+
