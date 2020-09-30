cf2d29001706c663e987eae2f9e55ee024ef2ce5
Jake Williams <jake5195@hotmail.co.uk>
2020-09-30 14:48:16 +0100

    compMoveSelect function created

--- app.js
+++ app.js
@@ -2,4 +2,14 @@ function playerMoveSelect() {
     let playerMove = prompt("Please select your move from the following; r, p or s");
     return playerMove
 }
+function compMoveSelect() {
+    let compVals = ["p", "r", "s"];
+    let randomVal = Math.floor(Math.random() * compVals.length);
+    let compVal = compVals[randomVal]
+    return compVal
+}
+
+
+
 console.log(playerMoveSelect());
+console.log(compMoveSelect());
